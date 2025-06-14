fe55f5cef266bcc566514303c065af45ddadd412
Buenaventura123 <202310536@fit.edu.ph>
2025-06-14 10:12:52 +0800

    fix: fix the paths

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,4 @@
 <?php
-define('BASE_PATH', '/Activity_1/'); // Adjust if your folder name is different
+define('BASE_PATH', '/AD-Activity-1/'); // Adjust if your folder name is different
 define('ROOT_PATH', value: __DIR__);
 ?>
