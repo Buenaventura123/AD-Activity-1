c9fd0ce5529e28bd1df2baec7be0df4c5cc2e9ff
Don Zaimone O. Miranda <saimone.don@gmail.com>
2025-07-05 08:31:35 +0800

    Add files via upload

---
+++ AD-Activity-1-refactor-bootstrap.php-compose.yaml-composer.json-alter-previous-code-for-docker-installation/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+define('BASE_PATH', realpath(__DIR__));
+// Adjust if your folder name is different
+chdir(BASE_PATH);
+?>
---
+++ AD-Activity-1-refactor-bootstrap.php-compose.yaml-composer.json-alter-previous-code-for-docker-installation/index.php
@@ -0,0 +1,20 @@
+<?php include_once './layout/main.layout.php'; ?>
+<?php include_once './components/componentGroup/navbar.component.php'; ?>
+<?php include_once './components/componentGroup/footer.component.php'; ?>
+
+<body>
+<nav>
+    <?php echo getNavbar('Home'); ?>
+</nav>
+
+<div class="main-content">
+    <h1 class="heading">GEMSTONE</h1>
+    <p class="minitext">
+        Discover the captivating beauty and unique properties of various gemstones, from their geological formation
+        to their cultural significance and uses in jewelry and art.
+    </p>
+</div>
+
+<script src="/page/home/assets/js/script.js"></script>
+
+<?php echo getFooter(); ?>
