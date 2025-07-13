61b2be82170fb5ca5ef2efc78211dfd8cc926f89
Buenaventura123 <202310536@fit.edu.ph>
2025-07-13 18:21:15 +0800

    feat: added static data for users

---
+++ staticData/user.staticData.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    [
+        'email' => 'user1@example.com',
+        'password' => 'password123',
+        'name' => 'User One',
+        'role' => 'user'
+    ],
+    [
+        'email' => 'admin@example.com',
+        'password' => 'admin123',
+        'name' => 'Admin User',
+        'role' => 'admin'
+    ],
+];
